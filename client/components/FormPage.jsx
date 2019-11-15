0b37d6819f2f71e8d4e9cf5265d58a6672ec3d87
Eloise McIntyre <eloise.mcintyre2@outlook.com>
2019-11-16 11:54:37 +1300

    added pictures and check boxes for the first 3 plants in the form page

--- client/components/FormPage.jsx
+++ client/components/FormPage.jsx
@@ -12,21 +12,49 @@ class FormPage extends React.Component {
         {/* Left blank */}
         <div className="col-md-3 col-sm-3 col-xs-3">
         </div>
-        
+
         <div className="col-md-6 col-sm-6 col-xs-6">
           <Form>
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Label>User Name</Form.Label>
+              <Form.Control type="email" placeholder="Enter User Name" />
             </Form.Group>
+            {/* <Form.Label>Image</Form.Label>
+            <img className="img-responsive" src="https://dummyimage.com/600x400/000/fff" />
+            <input name="image[]" value="" type="checkbox"> </input>
+            <i className="fa fa-check hidden"></i> */}
 
-            <Form.Group controlId="formBasicPassword">
+            {/* <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
+            </Form.Group> */}
+
+            <Form.Group controlId="formBasicCheckbox" style={{ textAlign: 'center' }}>
+              <Form.Check type="checkbox" label="Monstera Deliciosa" name="plantsOwned" value="monstera"/>
+              <img height="150" width="150" src="images/monstera.jpg" />
+              <br></br> <br></br>
+              <Form.Check type="checkbox" label="Zanzibar Gem" name="plantsOwned" value="zanzibar"/>
+              <img height="150" width="150" src="images/zanzibar.jpg" />
+              <br></br> <br></br>
+              <Form.Check type="checkbox" label="Pachira Aquatica" name="plantsOwned" value="pachira"/>
+              <img height="150" width="150" src="images/pachira.jpg" />
+              <br></br> <br></br>
             </Form.Group>
+
+            {/* <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="image" src="https://dummyimage.com/100x100/000/fff" />
+            </Form.Group>
+
             <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+              <Form.Check type="image" src="https://dummyimage.com/100x100/000/fff" />
             </Form.Group>
+
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="image" src="https://dummyimage.com/100x100/000/fff" />
+            </Form.Group> */}
+
+
+            {/* Neecd a redirect to the users profile page in here */}
             <Button variant="primary" type="submit">
             Submit
             </Button>
@@ -37,8 +65,10 @@ class FormPage extends React.Component {
         <div className="col-md-3 col-sm-3 col-xs-3">
         </div>
       </div>
+      <br></br>
+      <hr></hr>
     </div>
-    <hr></hr>
+
     </>
     )
   }
